d8b8c212512b834ab30724b7bf5c1881cbb9bdb4
SVO89 <dime4000@gmail.com>
2022-07-23 11:06:16 -0600

    Added aliases_singleRow.sql ddl_dml.sql views.sql

--- src/test/resources/queries/day04/aliases_singleRow.sql
+++ src/test/resources/queries/day04/aliases_singleRow.sql
@@ -28,3 +28,38 @@ select EMAIL||'@gmail.com' as full_email from EMPLOYEES;
 -- Another way
 select concat(EMAIL,'@GMAIL.COM') as full_email from EMPLOYEES;
 
+-- INIT CAP
+-- Makes the first letter uppercase
+select EMAIL
+
+-- LENGTH
+-- It will display the length of the provided data
+select length(EMAIL) from EMPLOYEES;
+-- Display all the employees first name and length of it; in the result order them based on length
+select FIRST_NAME,length(FIRST_NAME) as length_firstname from EMPLOYEES
+order by length(FIRST_NAME);
+
+-- By column aliases
+select FIRST_NAME,length(FIRST_NAME) as length_firstname from EMPLOYEES
+order by length_firstname;
+
+-- By column index
+select FIRST_NAME,length(FIRST_NAME) as length_firstname from EMPLOYEES
+order by 2;
+
+-- SUBSTR(colName,beginningIndex,numberOfChar)
+
+-- If beginningIndex is 0, it is treated as 1.
+-- If beginningIndex is negative, it will start from backwards of data.
+-- If numberOfChar is omitted, it works till the end.
+
+-- Display all initials from employees --> Steven King --> S.K.
+select substr(FIRST_NAME,0,1)||'.'||substr(LAST_NAME,0,1)||'.' from EMPLOYEES;
+
+select substr(FIRST_NAME,1,1)||'.'||substr(LAST_NAME,1,1)||'.' from EMPLOYEES;
+
+-- IQ from screenshot
+select FIRST_NAME,SALARY from EMPLOYEES
+where SALARY>3000
+order by lower(substr(FIRST_NAME,1)) asc,SALARY desc;
+
---
+++ src/test/resources/queries/day04/ddl_dml.sql
@@ -0,0 +1,84 @@
+select * from EMPLOYEES;
+
+-- CREATE TABLE
+
+/* Create table syntax:
+        create table TableName(
+            colName1 DataType Constraints,
+            colName2 DataType Constraints,
+            colName3 DataType Constraints,
+            ...
+        );
+ */
+
+CREATE TABLE scrumTeam(
+
+    employeeId Integer PRIMARY KEY, --> Not null and unique.
+    firstName varchar(30) not null, --> Can't be null.
+    jobTitle varchar(20) not null,  --> Can't be null.
+    age Integer,                    --> There is no constraints.
+    salary Integer
+);
+
+select * from SCRUMTEAM;
+
+/*
+ INSERT
+ Insert into table name (coulmn1, column2,...)
+ VALUES (value1,value2,...);
+ */
+
+INSERT INTO SCRUMTEAM(EMPLOYEEID,FIRSTNAME,JOBTITLE,AGE,SALARY)
+VALUES (1,'MIKE','TESTER',25,120000);
+
+INSERT INTO SCRUMTEAM(EMPLOYEEID,FIRSTNAME,JOBTITLE,AGE,SALARY)
+VALUES (2,'JOHN','TESTER',27,130000);
+
+INSERT INTO SCRUMTEAM(EMPLOYEEID,FIRSTNAME,JOBTITLE)
+VALUES (3,'DEMBE','DEVELOPER');
+
+select * from SCRUMTEAM;
+
+INSERT INTO SCRUMTEAM
+VALUES (4,'HENRY','SM',30,150000);
+
+/*
+ UPDATE
+ Update table name
+ SET column1 - value1,
+        column2 = value2,...
+ WHERE condition;
+ */
+
+-- Increase all employee's salary
+UPDATE SCRUMTEAM
+SET SALARY=SALARY+5000;
+
+select * from SCRUMTEAM;
+
+-- Increase salary for testers
+UPDATE SCRUMTEAM
+SET SALARY=SALARY+5000
+WHERE JOBTITLE='TESTER';
+
+select * from SCRUMTEAM;
+
+-- Add age for DEMBE
+UPDATE SCRUMTEAM
+SET AGE=34,SALARY=160000
+WHERE FIRSTNAME='DEMBE';
+
+SELECT * FROM SCRUMTEAM;
+
+/*
+ DELETE
+ Syntax: DELETE FROM TABLE_NAME
+         WHERE CONDITION;
+ */
+
+DELETE FROM SCRUMTEAM
+WHERE FIRSTNAME='DEMBE';
+
+SELECT * FROM SCRUMTEAM;
+
+COMMIT;
